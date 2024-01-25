e6176f554768ec5228ba22de68caeea76fec7880
cchen-oac <155970849+cchen-oac@users.noreply.github.com>
2024-01-25 12:47:08 +0000

    obtained all names and refs

--- Web_scrapping.py
+++ Web_scrapping.py
@@ -12,12 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# %%
 import streamlit as st
 from streamlit.logger import get_logger
 import matplotlib.pyplot as plt
 import pandas as pd
 import urllib.request as url
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup
 import openpyxl
 from zipfile import ZipFile
 import requests
@@ -42,6 +43,42 @@ def check_acess(url):
     else:
         notification = "Access to URL is denied!, please input a different link!"
     return notification    
+
+#Load all available csv from url and list out all dataset names
+@st.cache_data
+def load_dataset(html):
+    html = requests.get(url)    
+    #parse html info
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    #list of file extensions interested in
+    file_extensions = ['.xlsx', '.csv', '.pdf']
+
+    #extract all links available from html and their names
+    names = []
+    hrefs = []
+    for i in soup.find_all('a'):
+        link = i.get('href')
+        name = i.text.strip()  # remove leading/trailing white spaces
+        if link and any(link.endswith(ext) for ext in file_extensions):    # Only add link and name to the list if link is not None and ends with any of the file extensions
+            names.append(name)
+            hrefs.append(link)
+
+    #Combine both name and refs as dictionary
+    dict = {'names': names, 'hrefs': hrefs}
+    df = pd.DataFrame(dict).sort_values(by=['names']).reset_index(drop=True)
+
+    df = pd.DataFrame(list(zip(name_csv, links_csv)), columns=['Name', 'links']).sort_values(by=['Name']).reset_index(drop=True)  
+    #Save it as a table
+    #links_df = pd.DataFrame({'links':links_csv}).sort_values(by=['links'])  
+    return df
+
+
+
+dict = {'names': names, 'hrefs': hrefs}
+df = pd.DataFrame(dict)     
+# saving the dataframe
+df.to_csv('test.csv')
 
 
 
